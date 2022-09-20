a23189c3b628af93b08c5d607a4f3b584bbe1ef8
Tuananhmaii <anhmttgcd201452@fpt.edu.vn>
2022-09-20 16:11:35 +0700

    Done Section 7

--- BulkyBook.DataAccess/Data/ApplicationDbContext.cs
+++ BulkyBook.DataAccess/Data/ApplicationDbContext.cs
@@ -6,9 +6,10 @@ namespace BulkyBook.DataAccess
     public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
-        {
-            
+        {      
         }
         public DbSet<Category> Categories { get; set; }
+        public DbSet<CoverType> CoverTypes { get; set; }
+        public DbSet<Product> Products { get; set; }
     }
 }
